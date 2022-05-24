111ccf6b2784ba62255ccd1a6840104885e3f703
mercee20 <99300945+mercee20@users.noreply.github.com>
2022-05-23 21:39:49 -0300

    proyectoFinal

---
+++ proyectoFinal/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'components',
+    loadChildren: () => import('./app.module').then(m => m.AppModule)
+  },
+
+  {
+    path: '**',
+    redirectTo: 'components',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
